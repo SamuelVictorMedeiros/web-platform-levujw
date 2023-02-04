9bb5270d80b50b7db6ba9a5f35e9836ab30b1df2
SamuelVictorMedeiros <105554673+SamuelVictorMedeiros@users.noreply.github.com>
2023-02-03 22:45:10 -0300

    3 files modified

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
-
 let chave = "cebcd482eda57fa9a6714c1c2ba91885"
 
 function colocarnatela(dados){
    console.log(dados)
 
    document.querySelector(".cidade").innerHTML = "tempo em " + dados.name 
-
+   document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+   document.querySelector(".descricao").innerHTML = dados.weather[0].description
+   document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
 }
-
 
 async function buscarcidade(cidade){
     let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cidade + "&appid=" +  chave + "&lang=pt_br" + "&units=metric" )
